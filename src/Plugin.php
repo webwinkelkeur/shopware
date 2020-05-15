a17a972083a044eb0e04e7d34b53d33575f754e6
Albert Peschar <albert@peschar.net>
2020-05-15 16:14:23 +0300

    Plugin -> WebwinkelKeur

--- src/Plugin.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace WebwinkelKeur\Shopware;
-
-use Shopware\Core\Framework\Plugin as ShopwarePlugin;
-
-class Plugin extends ShopwarePlugin {
-}
---
+++ src/WebwinkelKeur.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace WebwinkelKeur\Shopware;
+
+use Shopware\Core\Framework\Plugin;
+
+class WebwinkelKeur extends Plugin {
+}
