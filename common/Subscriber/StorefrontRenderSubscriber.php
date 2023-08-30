ca121bd68f3b3eb316ed56e6832ee6a26bffaeab
Tsanko Hadzhiev <tsankohadzhiev@gmail.com>
2023-08-30 17:06:49 +0200

    Add signature ot order feed

--- common/Subscriber/StorefrontRenderSubscriber.php
+++ common/Subscriber/StorefrontRenderSubscriber.php
@@ -2,8 +2,10 @@
 
 namespace Valued\Shopware\Subscriber;
 
+use Shopware\Core\Checkout\Order\OrderEntity;
 use Shopware\Core\System\SystemConfig\SystemConfigService;
 use Shopware\Storefront\Event\StorefrontRenderEvent;
+use Shopware\Storefront\Page\Checkout\Finish\CheckoutFinishPage;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Valued\Shopware\Service\DashboardService;
 
@@ -31,6 +33,10 @@ class StorefrontRenderSubscriber implements EventSubscriberInterface {
             $sidebar_enabled = false;
         }
         if (!$event->getRequest()->isXmlHttpRequest()) {
+            $event->setParameter('_system_key', $this->dashboardService->getSystemKey());
+            if ($this->isThankYouPage($event)) {
+                $event->setParameter('_order_feed', $this->getOrderFeed($event->getParameters()['page']->getOrder(), $sales_channel_id));
+            }
             $event->setParameter(
                 '_shop_id',
                 $webshop_id,
@@ -43,9 +49,45 @@ class StorefrontRenderSubscriber implements EventSubscriberInterface {
                 '_dashboard_url',
                 sprintf('https://%s', $this->dashboardService->getDashboardHost()),
             );
-            $event->setParameter('_system_key', $this->dashboardService->getSystemKey());
-            $event->setParameter('_ask_for_consent', $this->dashboardService->getConfigValue('askForConsent', $sales_channel_id));
-            $event->setParameter('_invite_delay', $this->dashboardService->getConfigValue('delay', $sales_channel_id));
         }
     }
+
+    private function isThankYouPage(StorefrontRenderEvent $event): bool {
+        return ($event->getParameters()['page'] ?? null) instanceof CheckoutFinishPage;
+    }
+
+    private function getOrderFeed(OrderEntity $orderEntity, $sales_channel_id): string {
+        if (!$this->dashboardService->getConfigValue('askForConsent', $sales_channel_id)) {
+            return '';
+        }
+
+        $customer = $orderEntity->getOrderCustomer();
+        $order_feed = [
+            'webshopId' => $this->dashboardService->getConfigValue('webshopId', $sales_channel_id),
+            'orderNumber' => $orderEntity->getOrderNumber(),
+            'email' => $customer->getEmail(),
+            'firstName' => $customer->getFirstName(),
+            'inviteDelay' => $this->dashboardService->getConfigValue('delay', $sales_channel_id),
+        ];
+
+        $signature = hash_hmac(
+            'sha512',
+            http_build_query($order_feed),
+            $this->getHashKey($sales_channel_id)
+        );
+
+        $order_feed['signature'] = $signature;
+
+        return json_encode($order_feed, JSON_HEX_QUOT | JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS);
+    }
+
+
+    private function getHashKey($sales_channel_id): string {
+        return sprintf(
+            '%s:%s',
+            $this->dashboardService->getConfigValue('webshopId', $sales_channel_id),
+            $this->dashboardService->getConfigValue('apiKey', $sales_channel_id)
+        );
+    }
+
 }
