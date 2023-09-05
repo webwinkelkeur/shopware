afe236475e782528d42071463036433daa1d0b14
Tsanko Hadzhiev <tsankohadzhiev@gmail.com>
2023-09-05 15:43:04 +0300

    Add typehint

--- common/Twig/ConsentData.php
+++ common/Twig/ConsentData.php
@@ -15,7 +15,7 @@ class ConsentData extends AbstractExtension {
         $this->dashboardService = $dashboardService;
     }
 
-    public function getFunctions() {
+    public function getFunctions(): array {
         return [
             new TwigFunction('getConsentOrderData', [$this, 'getConsentOrderData']),
         ];
