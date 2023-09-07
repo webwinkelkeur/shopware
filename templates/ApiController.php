a5c390130fea778b469cedfd4357171c9648e6b8
Tsanko Hadzhiev <tsankohadzhiev@gmail.com>
2023-09-07 09:43:17 +0300

    Formatting

--- templates/ApiController.php
+++ templates/ApiController.php
@@ -35,21 +35,20 @@ class {SYSTEM_NAME}ApiController extends StorefrontController {
     function check(RequestDataBag $dataBag): JsonResponse {
         $webshopId = $dataBag->get(sprintf('%s.config.webshopId', $this->dashboardService->getSystemName()));
         $apiKey = $dataBag->get(sprintf('%s.config.apiKey', $this->dashboardService->getSystemName()));
+
         $base_url = sprintf('https://%s/api/1.0/webshop.json', $this->dashboardService->getDashboardHost());
         $params = http_build_query([
             'id' => $webshopId,
             'code' => $apiKey,
         ]);
-
         $url = sprintf('%s?%s', $base_url, $params);
-        $result = @file_get_contents($url);
 
+        $result = @file_get_contents($url);
         if (!empty($result)) {
             $result = json_decode($result, true);
         }
 
-        $status = $result['status'] ?? null;
-
-        return new JsonResponse(['success' => $status === 'success']);
+        $status = ($result['status'] ?? null) === 'success';
+        return new JsonResponse(['success' => $status]);
     }
 }
